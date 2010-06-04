28aad50788ee3299e913d28c7410545233b17a84
Patrick Galbraith <patg@northscale.com>
2010-06-04 08:36:02 -0400

    Working condept of dictionary of classes

--- membase.py
+++ membase.py
@@ -1,23 +1,33 @@
-/*
-  membase_cli.cc
+#!/usr/bin/env python
+"""
+  membase.py
 
   This program is the top level source file for the Membase Command Line Tools
 
-  ... more docs to come ...
-*/
+"""
+import getopt,sys
+from membase_cli_* import *
 
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <tclap/CmdLine.h>
 
-using namespace std;
+def usage():
+  print >> sys.stderr, "Usage: membase command [OPTIONS]"
 
-class MembaseCli {
-public:
-  virtual MembaseCli () {}
-  virtual string runCmd () {}
-  virtual ~MembaseCli () {}
-}
+if __name__ == "__main__":
+  try:
+    optlist, args = getopt.getopt(sys.argv[2:], 'b:c:e:gdp:c:p:s:')
+    cmd = sys.argv[1]
+    print "command: %s" % cmd 
+    optlist
+  except  getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+
+  commands = { 'listservers' : Listservers}
+
+  if cmd not in commands:
+    usage()
+
+  taskrunner = commands[cmd](optlist)
+  taskrunner.runCmd(cmd)
 
 
---
+++ membase_cli_listservers.py
@@ -0,0 +1,17 @@
+"""
+  listservers class
+
+  This class implements methods that will list servers within a 
+  membase cluster
+
+"""
+
+class Listservers:
+  def __init__(self, options):
+    """ 
+      constructor
+    """
+
+  def runCmd(self, cmd):
+    print "Running %s" % cmd
+
