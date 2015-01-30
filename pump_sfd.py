1686e33d504b1172b778c61334b436055aff7aa3
Bin Cui <bin.cui@gmail.com>
2015-01-30 11:18:57 -0800

    MB-13216: Interpret conflict flag
    Change-Id: Ic2d73c01b476b72cbb3bd42ba2aad8abd8daad99
    Reviewed-on: http://review.couchbase.org/46144
    Reviewed-by: Bin Cui <bin.cui@gmail.com>
    Tested-by: Bin Cui <bin.cui@gmail.com>

--- pump_sfd.py
+++ pump_sfd.py
@@ -17,7 +17,7 @@ from cbqueue import PumpQueue
 
 SFD_SCHEME = "couchstore-files://"
 SFD_VBUCKETS = 1024
-SFD_REV_META = ">QIIBB" # cas, exp, flg, flex_meta, dtype
+SFD_REV_META = ">QIIBBB" # cas, exp, flg, flex_meta, dtype, conf_res
 SFD_REV_SEQ = ">Q"
 SFD_DB_SEQ = ">Q"
 SFD_RE = "^([0-9]+)\\.couch\\.([0-9]+)$"
@@ -215,7 +215,7 @@ class SFDSource(pump.Source):
                     cmd = couchbaseConstants.CMD_TAP_MUTATION
                     val = doc_info.getContents(options=couchstore.CouchStore.DECOMPRESS)
 
-                cas, exp, flg, flex_meta, dtype = struct.unpack(SFD_REV_META, doc_info.revMeta)
+                cas, exp, flg, flex_meta, dtype, conf_res = struct.unpack(SFD_REV_META, doc_info.revMeta)
                 meta = doc_info.revSequence
                 seqno = doc_info.sequence
                 nmeta = 0
@@ -288,7 +288,8 @@ class SFDSink(pump.Sink):
 
                     d = couchstore.DocumentInfo(str(key))
                     flex_meta = 1
-                    d.revMeta = str(struct.pack(SFD_REV_META, cas, exp, flg, flex_meta, dtype))
+                    conf_res = 0
+                    d.revMeta = str(struct.pack(SFD_REV_META, cas, exp, flg, flex_meta, dtype, conf_res))
                     if meta:
                         if len(meta) > 8:
                             meta = meta[0:8]
