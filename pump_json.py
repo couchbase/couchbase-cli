c892c9241d1e6997fa30317af791d6fcde73aeaa
Mike Wiederhold <mikewied06@gmail.com>
2016-03-05 17:17:32 -0800

    Refactor json loader in order to improve performance
    The current json loader would extract the json zip file twice in
    order to load data and this was problematic because on Windows it
    can take up to 3.5 minutes to unzip the file.
    
    The new code does not extract the contents of the zip file and
    instead reads directly from the zip files.
    
    Change-Id: I764d0538dedea69adc60e5b587a1ab2f7722e359
    Reviewed-on: http://review.couchbase.org/60988
    Reviewed-by: Eben Haber <eben@couchbase.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- pump_json.py
+++ pump_json.py
@@ -14,52 +14,6 @@ import pump
 
 JSON_SCHEME = "json://"
 
-class ZipUtil:
-    def __init__(self, zipobj):
-        self.zipobj = zipobj
-
-    def extractall(self, path=None):
-        if path is None:
-            path = os.getcwd()
-        if (path[-1] in (os.path.sep, os.path.altsep)
-            and len(os.path.splitdrive(path)[1]) > 1):
-            path = path[:-1]
-
-        for member in self.zipobj.namelist():
-            if not isinstance(member, zipfile.ZipInfo):
-                member = self.zipobj.getinfo(member)
-
-            # don't include leading "/" from file name if present
-            if member.filename[0] == '/':
-                targetpath = os.path.join(path, member.filename[1:])
-            else:
-                targetpath = os.path.join(path, member.filename)
-
-            targetpath = os.path.normpath(targetpath)
-
-            # Create all parent directories if necessary.
-            upperdirs = os.path.dirname(targetpath)
-            if upperdirs and not os.path.exists(upperdirs):
-                try:
-                    os.makedirs(upperdirs)
-                except:
-                    logging.error("Unexpected error:" + sys.exc_info()[0])
-                    return upperdirs
-
-            if member.filename[-1] == '/':
-                if not os.path.isdir(targetpath):
-                    try:
-                        os.mkdir(targetpath)
-                    except:
-                        logging.error("Fail to create directory:" + targetpath)
-                continue
-
-            target = file(targetpath, "wb")
-            target.write(self.zipobj.read(member.filename))
-            target.close()
-
-        return path
-
 class JSONSource(pump.Source):
     """Reads json file or directory or zip file that contains json files."""
 
@@ -68,9 +22,8 @@ class JSONSource(pump.Source):
         super(JSONSource, self).__init__(opts, spec, source_bucket, source_node,
                                          source_map, sink_map, ctl, cur)
         self.done = False
-        self.f = None
+        self.docs = list()
         self.file_iter = None
-        self.working_dir = None
 
     @staticmethod
     def can_handle(opts, spec):
@@ -85,17 +38,15 @@ class JSONSource(pump.Source):
                    'buckets': [{'name': os.path.basename(spec),
                                 'nodes': [{'hostname': 'N/A'}]}]}
 
-    def save_doc(self, batch, dockey, datafile, is_data):
+    def save_doc(self, batch, dockey, docvalue):
         cmd = couchbaseConstants.CMD_TAP_MUTATION
         vbucket_id = 0x0000ffff
         cas, exp, flg = 0, 0, 0
         try:
-            raw_data = datafile.read()
-            doc = json.loads(raw_data)
+            doc = json.loads(docvalue)
             if '_id' not in doc:
-                if is_data:
-                    msg = (cmd, vbucket_id, dockey, flg, exp, cas, '', raw_data, 0, 0, 0, 0)
-                    batch.append(msg, len(raw_data))
+                msg = (cmd, vbucket_id, dockey, flg, exp, cas, '', docvalue, 0, 0, 0, 0)
+                batch.append(msg, len(docvalue))
             else:
                 id = doc['_id'].encode('UTF-8')
                 del doc['_id']
@@ -114,68 +65,47 @@ class JSONSource(pump.Source):
 
     @staticmethod
     def enumerate_files(subdir, file_candidate, skip_views, skip_docs):
-        if not subdir:
-            return
-        subdirlist = list()
-        viewdirs = list()
-        indexdirs = list()
         for item in os.listdir(subdir):
             path = os.path.join(subdir, item)
             if os.path.isfile(path):
-                if (not skip_views and path.find("/design_docs") > 0) or \
-                   (not skip_docs and path.find("/docs") > 0):
+                dir = os.path.basename(os.path.dirname(path))
+                if (not skip_views and dir == "design_docs") or \
+                   (not skip_docs and dir == "docs"):
                     file_candidate.append(path)
             else:
-                if item.find("/design_docs") > 0:
-                        viewdirs.append(path)
-                else:
-                    subdirlist.append(path)
-        for dir in subdirlist:
-            JSONSource.enumerate_files(dir, file_candidate, skip_views, skip_docs)
-        for dir in viewdirs:
-            JSONSource.enumerate_files(dir, file_candidate, skip_views, skip_docs)
-
-    def build_batch(self, batch, is_data, batch_max_size):
-        for path in self.file_iter:
-            if os.path.isfile(path):
-                try:
-                    fp = open(path, 'r')
-                    dockey = JSONSource.gen_dockey(os.path.basename(path))
-                    self.save_doc(batch, dockey, fp, is_data)
-                    fp.close()
-                except IOError, error:
-                    logging.error("Fail to load json file with error" + str(error))
-                if batch.size() >= batch_max_size:
-                    return batch
-
-        #at end of enumeration, clean up working directory
-        self.done = True
-        if self.working_dir:
-            shutil.rmtree(self.working_dir)
-        return batch
+                dir = os.path.basename(path)
+                if not ((skip_docs and dir == "docs") or \
+                   (skip_views and dir == "design_docs")):
+                    JSONSource.enumerate_files(path, file_candidate, skip_views, skip_docs)
 
     @staticmethod
     def provide_design(opts, source_spec, source_bucket, source_map):
+        design_files = list()
         f = source_spec.replace(JSON_SCHEME, "")
-        files = list()
-        working_dir = None
+
         if os.path.isfile(f) and f.endswith(".zip"):
-            zfobj = zipfile.ZipFile(f)
-            working_dir = tempfile.mkdtemp()
-            ZipUtil(zfobj).extractall(working_dir)
-            JSONSource.enumerate_files(working_dir, files, False, True)
+            zf = zipfile.ZipFile(f)
+            for path in zf.namelist():
+                file = os.path.basename(path)
+                # Skip the design_docs directory listing
+                if file == "design_docs":
+                    continue
+
+                dir = os.path.basename(os.path.dirname(path))
+                # Skip all files not in the design docs directory
+                if dir != "design_docs":
+                    continue
+
+                design_files.append(zf.read(path))
+            zf.close()
         elif os.path.isdir(f):
-            JSONSource.enumerate_files(working_dir, files, False, True)
-
-        design_files = []
-        for path in files:
-            if os.path.isfile(path):
-                f = open(path, 'r')
-                design_files.append(f.read())
-                f.close()
-
-        if working_dir:
-            shutil.rmtree(working_dir)
+            files = list()
+            JSONSource.enumerate_files(f, files, False, True)
+            for path in files:
+                if os.path.isfile(path):
+                    f = open(path, 'r')
+                    design_files.append(f.read())
+                    f.close()
 
         return 0, design_files
 
@@ -183,32 +113,58 @@ class JSONSource(pump.Source):
         if self.done:
             return 0, None
 
+        # During the first iteration load the file names, this is only run once
+        if not self.docs:
+            self.prepare_docs()
+
         batch = pump.Batch(self)
-        if self.file_iter == None:
-            self.f = self.spec.replace(JSON_SCHEME, "")
-            files = list()
-            if os.path.isfile(self.f) and self.f.endswith(".zip"):
-                zfobj = zipfile.ZipFile(self.f)
-                self.working_dir = tempfile.mkdtemp()
-                ZipUtil(zfobj).extractall(self.working_dir)
-                JSONSource.enumerate_files(self.working_dir, files, True, False)
-            elif os.path.isdir(self.f):
-                JSONSource.enumerate_files(self.f, files, True, False)
-            else:
+        f = self.spec.replace(JSON_SCHEME, "")
+        batch_max_size = self.opts.extra['batch_max_size']
+
+        # Each iteration should return a batch or mark the loading a finished
+        if os.path.isfile(f) and f.endswith(".zip"):
+            zf = zipfile.ZipFile(f)
+            while batch.size() < batch_max_size and self.docs:
+                path = self.docs.pop()
+                key = os.path.basename(path)
+                value = zf.read(path)
+                self.save_doc(batch, key, value)
+            zf.close()
+        else:
+            while batch.size() < batch_max_size and self.docs:
+                path = self.docs.pop()
+                key = os.path.basename(path)
                 try:
-                    fp = open(self.f, 'r')
-                    dockey = JSONSource.gen_dockey(os.path.basename(self.f))
-                    self.save_doc(batch, dockey, fp, True)
+                    fp = open(path, 'r')
+                    value = fp.read()
                     fp.close()
+                    self.save_doc(batch, key, value)
                 except IOError, error:
-                    return "error: could not open json: %s; exception: %s" % \
-                        (self.f, e), None
-                self.done = True
-                return 0,batch
-            if len(files) > 0:
-                self.file_iter = iter(files)
+                    logging.error("Fail to load json file with error" + str(error))
 
-        if self.file_iter:
-            return 0, self.build_batch(batch, True, self.opts.extra['batch_max_size'])
+        if not self.docs:
+            self.done = True
 
-        return 0, None
+        return 0, batch
+
+    def prepare_docs(self):
+        f = self.spec.replace(JSON_SCHEME, "")
+        if os.path.isfile(f) and f.endswith(".zip"):
+            zf = zipfile.ZipFile(f)
+            for path in zf.namelist():
+                file = os.path.basename(path)
+                # Skip the docs directory listing
+                if file == "docs":
+                    continue
+
+                dir = os.path.basename(os.path.dirname(path))
+                # Skip all files not in the docs directory
+                if dir != "docs":
+                    continue
+
+                self.docs.append(path)
+            zf.close()
+        elif os.path.isdir(f):
+            JSONSource.enumerate_files(f, self.docs, True, False)
+        else:
+            self.docs.append(f)
