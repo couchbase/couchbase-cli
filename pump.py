6539760501338c552673702ef0e0ce7a6a5f8eb6
Patrick Varley <patrick@couchbase.com>
2018-06-05 11:14:04 +0100

    MB-29972 Fetch design docs for membase buckets only
    Before this change cbbackup would fail on ephemeral buckets when
    it tried to incorrectly fetch the design document.
    
    Change-Id: I21763790a4a5303be6797f7899c700d3a7af690a
    Reviewed-on: http://review.couchbase.org/95180
    Tested-by: Patrick Varley <patrick@couchbase.com>
    Reviewed-by: Simon Murray <simon.murray@couchbase.com>

--- pump.py
+++ pump.py
@@ -140,9 +140,10 @@ class PumpingStation(ProgressReporter):
                 sys.stderr.write("transfer design doc only. bucket msgs will be skipped.\n")
 
             if not self.opts.extra.get("data_only", 0):
-                rv = self.transfer_bucket_design(source_bucket, source_map, sink_map)
-                if rv:
-                    logging.warn(rv)
+                if source_bucket['bucketType'] == 'membase':
+                    rv = self.transfer_bucket_design(source_bucket, source_map, sink_map)
+                    if rv:
+                        logging.warn(rv)
                 rv = self.transfer_bucket_index(source_bucket, source_map, sink_map)
                 if rv:
                     logging.warn(rv)
